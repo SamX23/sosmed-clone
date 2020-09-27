f3e1057b1cf8bc65d04083346d8f63d44a3a4d97
Sami Kalammallah <skalexsong@gmail.com>
2020-09-27 20:59:57 +0700

    update

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ Technology used :
 - Flexbox
 - React flip move
 - Firebase (DB, hosting, auth)
+
+
+<!-- npm install -g firebase-tools -->
+<!-- firebase init -->
+<!-- {
+  "hosting": {
+    "site": "facebook-clone-sam",
+    "public": "public",
+    ...
+  }
+} -->
+<!-- firebase deploy --only hosting:facebook-clone-sam -->
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/600px-Facebook_f_logo_%282019%29.svg.png"
+          alt=""
+        />
+      </div>
+
+      <div className="header__middle"></div>
+
+      <div className="header__right"></div>
+    </div>
+  );
+}
+
+export default Header;
